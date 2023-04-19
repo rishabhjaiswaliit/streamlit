9dd2c31a4417c9153a16e909549850fc66eb5e7d
Simple Shadow <simpleshadow@Simples-MacBook-Pro.local>
2023-04-19 23:42:42 +0530

    feature: find max of three numbers

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.subheader("UseCase: Find the largest among the 3 given numbers(value greater than the other two).")
+st.title("Finding the largest among the 3 given numbers")
+number1 = st.number_input('Insert a number')
+number2 = st.number_input('Insert another number')
+number3 = st.number_input('Insert yet another number')
+
+st.write('The greatest number is: ', max(number1, number2, number3))  
